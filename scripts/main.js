7e8e1238296953786b738e4d235fee86568ababf
Shan1024 <gambit1024@gmail.com>
2015-05-18 00:21:59 +0530

    Edit/Remove features added

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,7 @@ $(function(){
   var $bearsList = $('#bears');
   var $name = $('#name');
 
-  var orderTemplate = "<li>"+
-  "Name: {{name}}"+
-  "<button data-id='{{_id}}' class='remove'>X</button>"+
-  "</li>";
+  var orderTemplate = $('#bear-template').html();
 
   $.ajax ({
     type: 'GET',
@@ -37,7 +34,7 @@ $(function(){
       data: temp,
       success: function(newBear){
         console.log(newBear);
-        $bearsList.append('<li>Name: '+newBear.name+'</li>');
+        $bearsList.append(Mustache.render(orderTemplate, newBear));
       },
       error: function(){
         alert('Error saving bear');
@@ -52,12 +49,49 @@ $(function(){
 
     $.ajax({
       type: 'DELETE',
-      url: 'api/bears/'+$(this).attr('data-id'),
+      url: 'api/bears/'+$li.attr('data-id'),
       success: function(){
-        $li.remove();
+        $li.fadeOut(300, function(){
+          $(this).remove();
+        });
       },
       error: function(){
 
+      }
+    });
+
+  });
+
+  $bearsList.delegate('.editBear', 'click', function(){
+    var $li = $(this).closest('li');
+    $li.find('input.name').val($li.find('span.name').html());
+    $li.addClass('edit');
+  });
+
+  $bearsList.delegate('.cancelEdit', 'click', function(){
+    var $li = $(this).closest('li');
+    $li.removeClass('edit');
+  });
+
+  $bearsList.delegate('.saveEdit', 'click', function(){
+
+    var $li = $(this).closest('li');
+
+    var temp = {
+      name: $li.find('input.name').val()
+    };
+
+    $.ajax({
+      type: 'PUT',
+      url: '/api/bears/'+$li.attr('data-id'),
+      data: temp,
+      success: function(newBear){
+        console.log(newBear);
+        $li.find('span.name').html(newBear.name);
+        $li.removeClass('edit');
+    },
+      error: function(){
+        alert('Error updating bear');
       }
     });
 
